831ff6f9ace152964d398d40b371822b3a672165
Jan van Mansum <janvanmansum@gmail.com>
2023-03-01 18:40:14 +0100

    Added example for creating a dataset

---
+++ examples/src/main/java/nl/knaw/dans/lib/dataverse/example/DataverseCreateDataset.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2021 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.lib.dataverse.example;
+
+import nl.knaw.dans.lib.dataverse.CompoundFieldBuilder;
+import nl.knaw.dans.lib.dataverse.DataverseHttpResponse;
+import nl.knaw.dans.lib.dataverse.ExampleBase;
+import nl.knaw.dans.lib.dataverse.model.dataset.ControlledMultiValueField;
+import nl.knaw.dans.lib.dataverse.model.dataset.Dataset;
+import nl.knaw.dans.lib.dataverse.model.dataset.DatasetCreationResult;
+import nl.knaw.dans.lib.dataverse.model.dataset.DatasetVersion;
+import nl.knaw.dans.lib.dataverse.model.dataset.MetadataBlock;
+import nl.knaw.dans.lib.dataverse.model.dataset.MetadataField;
+import nl.knaw.dans.lib.dataverse.model.dataset.PrimitiveSingleValueField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DataverseCreateDataset extends ExampleBase {
+
+    private static final Logger log = LoggerFactory.getLogger(DataverseCreateDataset.class);
+
+    public static void main(String[] args) throws Exception {
+        MetadataBlock citation = new MetadataBlock();
+        citation.setName("citation");
+        citation.setDisplayName("Citation Metadata");
+
+        MetadataField title = new PrimitiveSingleValueField("title", "Test dataset");
+        MetadataField description = new CompoundFieldBuilder("dsDescription", true)
+            .addSubfield("dsDescriptionValue", "Test description")
+            .addSubfield("dsDescriptionDate", "").build();
+        MetadataField author = new CompoundFieldBuilder("author", true)
+            .addSubfield("authorName", "A U Thor")
+            .addSubfield("authorAffiliation", "Walhalla").build();
+        MetadataField contact = new CompoundFieldBuilder("datasetContact", true)
+            .addSubfield("datasetContactName", "Test Contact")
+            .addSubfield("datasetContactEmail", "test@example.com").build();
+        MetadataField subjects = new ControlledMultiValueField("subject", Arrays.asList("Arts and Humanities", "Computer and Information Science"));
+
+        citation.setFields(Arrays.asList(title, author, contact, description, subjects));
+
+        DatasetVersion version = new DatasetVersion();
+        version.setMetadataBlocks(Collections.singletonMap("citation", citation));
+        version.setFiles(Collections.emptyList());
+
+        Dataset dataset = new Dataset();
+        dataset.setDatasetVersion(version);
+
+        log.info(mapper.writeValueAsString(dataset));
+
+        DataverseHttpResponse<DatasetCreationResult> r = client.dataverse("root").createDataset(dataset);
+
+        log.info("--- END JSON OBJECT ---");
+        log.info("Status Line: {}", r.getHttpResponse().getStatusLine());
+        log.info("DOI: {}", r.getData().getPersistentId());
+    }
+}
